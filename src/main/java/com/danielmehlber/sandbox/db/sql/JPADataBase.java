7cc8563732a0cd58d4f4df16c7ae5e5aab5cf23f
Daniel Mehlber <daniel.mehlber@gmail.com>
2021-12-11 00:11:55 +0100

    catch runtime exception to prevent bugs on nullpointer or other unexpected occurences

--- src/main/java/com/danielmehlber/sandbox/db/sql/JPADataBase.java
+++ src/main/java/com/danielmehlber/sandbox/db/sql/JPADataBase.java
@@ -6,9 +6,7 @@ import com.danielmehlber.sandbox.entities.Person;
 import com.danielmehlber.sandbox.exceptions.InternalErrorException;
 import com.danielmehlber.sandbox.exceptions.NoSuchPersonException;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -42,31 +40,39 @@ public class JPADataBase implements DataBaseConnection {
 
     @Override
     public void disconnect() throws DataBaseException {
-        if(applicationManaged == true)
-            entityManager.close();
+        try {
+            if (applicationManaged == true)
+                entityManager.close();
+        } catch (RuntimeException e) {
+            throw new DataBaseException("cannot disconnect", e);
+        }
     }
 
     private void createSchemaInCurrentDataBase() throws InternalErrorException {
-        // setup.sql contains database model
-        BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/setup.sql")));
-        String setupQuery = "";
-
-        // read contents of setup.sql
         try {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                setupQuery += line + "\n";
-            }
-        } catch (IOException e) {
-            throw new InternalErrorException("cannot read setup.sql", e);
-        }
+            // setup.sql contains database model
+            BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/setup.sql")));
+            String setupQuery = "";
 
-        entityManager.getTransaction().begin();
-        entityManager.createNativeQuery(setupQuery).executeUpdate();
-        entityManager.getTransaction().commit();
+            // read contents of setup.sql
+            try {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    setupQuery += line + "\n";
+                }
+            } catch (IOException e) {
+                throw new InternalErrorException("cannot read setup.sql", e);
+            }
+
+            entityManager.getTransaction().begin();
+            entityManager.createNativeQuery(setupQuery).executeUpdate();
+            entityManager.getTransaction().commit();
+        } catch (RuntimeException e) {
+            throw new InternalErrorException("cannot create schema on database", e);
+        }
     }
 
-    private void createTestDatabase() {
+    private void createTestDatabase() throws RuntimeException{
         /*
          * native queries need manual transaction management
          */
@@ -75,7 +81,7 @@ public class JPADataBase implements DataBaseConnection {
         entityManager.getTransaction().commit();
     }
 
-    private void dropTestDataBaseIfExists() {
+    private void dropTestDataBaseIfExists() throws RuntimeException{
         /*
          * native queries need manual transaction management
          */
@@ -104,53 +110,73 @@ public class JPADataBase implements DataBaseConnection {
 
     @Override
     public void closeTestConnection() throws DataBaseException, InternalErrorException {
-        if(entityManager != null && entityManager.isOpen())
-            entityManager.close();
+        try {
+            if (entityManager != null && entityManager.isOpen())
+                entityManager.close();
+        } catch (RuntimeException e) {
+            throw new DataBaseException("Cannot close test connection", e);
+        }
     }
 
     @Override
     public void prepareForTest() throws InternalErrorException {
-        // 1: disconnect from remaining connections
-        if(entityManager.isOpen())
+        try {
+            // 1: disconnect from remaining connections
+            if (entityManager.isOpen())
+                entityManager.close();
+
+            // 2: Connect without database in order to delete and recreate 'test' database
+            Map<String, String> properties = new HashMap<String, String>();
+            properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306");
+            entityManager = Persistence.createEntityManagerFactory("test", properties).createEntityManager();
+
+            // 3: drop database if it exists (relevant at initial start) and create new one
+            dropTestDataBaseIfExists();
+            createTestDatabase();
             entityManager.close();
 
-        // 2: Connect without database in order to delete and recreate 'test' database
-        Map<String, String> properties = new HashMap<String, String>();
-        properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306");
-        entityManager = Persistence.createEntityManagerFactory("test", properties).createEntityManager();
+            // 4: Connect to database 'test' and create schema
+            properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/test");
+            entityManager = Persistence.createEntityManagerFactory("test", properties).createEntityManager();
 
-        // 3: drop database if it exists (relevant at initial start) and create new one
-        dropTestDataBaseIfExists();
-        createTestDatabase();
-        entityManager.close();
-
-        // 4: Connect to database 'test' and create schema
-        properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/test");
-        entityManager = Persistence.createEntityManagerFactory("test", properties).createEntityManager();
-
-        // load setup.sql and create schema in current database (which is the newly created 'test' database)
-        createSchemaInCurrentDataBase();
+            // load setup.sql and create schema in current database (which is the newly created 'test' database)
+            createSchemaInCurrentDataBase();
+        } catch (RuntimeException e) {
+            throw new InternalErrorException("Cannot prepare for test", e);
+        }
     }
 
     @Override
     public void cleanupAfterTest() throws DataBaseException, InternalErrorException {
-        dropTestDataBaseIfExists();
-        closeTestConnection();
+        try {
+            dropTestDataBaseIfExists();
+            closeTestConnection();
+        } catch (RuntimeException e) {
+            throw new InternalErrorException("cannot clean up after test", e);
+        }
     }
 
     @Override
     public List<Person> fetchPersonByName(String name) throws DataBaseException, InternalErrorException {
-        return entityManager    .createQuery("SELECT * FROM Person p WHERE p.name=:name", Person.class)
-                                .setParameter("name", name)
-                                .getResultList();
+        try {
+            return entityManager.createQuery("SELECT * FROM Person p WHERE p.name=:name", Person.class)
+                    .setParameter("name", name)
+                    .getResultList();
+        } catch (RuntimeException e) {
+            throw new InternalErrorException("Cannot fetch person by name", e);
+        }
     }
 
     @Override
     public Person fetchById(int id) throws DataBaseException, NoSuchPersonException {
-        Person person =  entityManager.find(Person.class, id);
-        if(person == null)
-            throw new NoSuchPersonException(id);
-        return person;
+        try {
+            Person person = entityManager.find(Person.class, id);
+            if (person == null)
+                throw new NoSuchPersonException(id);
+            return person;
+        } catch (RuntimeException e) {
+            throw new DataBaseException("cannot fetch person by id", e);
+        }
     }
 
     @Override
@@ -159,7 +185,7 @@ public class JPADataBase implements DataBaseConnection {
         try {
             entityManager.persist(person);
         } catch (RuntimeException e) {
-            throw new DataBaseException("Cannot persist person", e);
+            throw new InternalErrorException("Cannot persist person", e);
         }
     }
 
